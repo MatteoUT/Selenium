1e4bf73020af7f705728159424100f29f7a103a5
MatteoUT <mattpuccini@utexas.edu>
2024-09-13 14:22:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,19 @@ try:
     # Navigate to the login page
     driver.get("https://poe.com/login")
     
-    # Click the "Use phone" button
-    use_phone_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Use phone')]")
+    # Click the "Use phone" button using the class name
+    use_phone_button = driver.find_element(By.CLASS_NAME, "Button_flat__dcKQ1")
     use_phone_button.click()
     
     # Wait for the phone number input to be visible
     time.sleep(2)  # Adjust the sleep time as necessary for your network speed
     
-    # Enter the phone number
-    phone_input = driver.find_element(By.XPATH, "//input[@placeholder='Phone number']")
-    phone_input.send_keys("18338191679")
+    # Enter the phone number using the class name for the input element
+    phone_input = driver.find_element(By.CLASS_NAME, "textInput_input__9YpqY")
+    phone_input.send_keys("18312210562")
     
-    # Click the "Go" button
-    go_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Go')]")
+    # Click the "Go" button using the class name
+    go_button = driver.find_element(By.CLASS_NAME, "Button_primary__6UIn0")
     go_button.click()
     
     # Additional steps can be added here if needed, such as handling the next page or verifying login success
@@ -38,3 +38,4 @@ try:
 finally:
     # Close the WebDriver
     driver.quit()
+18312210562
